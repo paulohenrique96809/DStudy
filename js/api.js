778f16042ba26a8304e3f044235e3adaa7a19864
paulohenrique96809 <paulohenrique0096809@gmail.com>
2026-08-18 17:31:36 -0300

    Criado a variável API_BASE_URL para quando o flask estiver rodando.

--- js/api.js
+++ js/api.js
@@ -1,1 +1,5 @@
+// URL base da API - quando o Flask estiver rodando, será algo como:
+// const API_BASE_URL = 'http://localhost:5000/api';
+// Por enquanto, deixamos vazio para testes sem servidor
+const API_BASE_URL = '';
 
